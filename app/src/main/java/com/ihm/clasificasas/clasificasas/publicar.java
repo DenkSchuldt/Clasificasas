71c2fc3b1166f5230bcac620ef173e555ed8930f
Kakrain <wherethesunnoshine@hotmail.com>
2014-09-05 15:12:28 -0500

    grid de imagenes y obtener path de una imagen...
    grid de imagenes y obtener path de una imagen ya sea de la galeria o de
    la camara

--- app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
+++ app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
@@ -2,13 +2,24 @@ package com.ihm.clasificasas.clasificasas;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+
+import java.io.File;
 
 /**
  * Created by Denny on 28/07/2014.
@@ -16,7 +27,11 @@ import android.widget.Spinner;
 public class publicar extends Activity {
 
     Spinner spinner_tipo;
-
+    private Uri mImageCaptureUri;
+    private Bitmap ImagenActual;
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_FILE = 2;
+    String path="";
     String[] tipo = {
             "Venta / Alquiler", "Venta", "Alquiler"
     };
@@ -27,7 +42,7 @@ public class publicar extends Activity {
         setContentView(R.layout.publicar);
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-
+        prepareImagePicker();
         /*spinner_tipo = (Spinner)findViewById(R.id.publicar_tipo_sp);
         ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tipo);
         spinner_tipo.setAdapter(adapter4);
@@ -59,4 +74,92 @@ public class publicar extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
-}
+    public void prepareImagePicker() {
+        final String[] items = new String[]{"From Camera", "From SD Card"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Select Image");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                if (item == 0) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File file = new File(Environment.getExternalStorageDirectory(),
+                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    mImageCaptureUri = Uri.fromFile(file);
+
+                    try {
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+                        intent.putExtra("return-data", true);
+
+                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    dialog.cancel();
+                } else {
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        });
+        final AlertDialog dialog = builder.create();
+
+        findViewById(R.id.subir_foto).setOnClickListener(new View.OnClickListener() {
+
+            //mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+                ((ImageView)findViewById(R.id.subir_foto)).setImageBitmap(ImagenActual);
+            }
+        });
+        findViewById(R.id.publicar_fotografias).setOnClickListener(new View.OnClickListener() {
+            //mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+    }
+    public String getRealPathFromURI(Uri contentUri) {
+        String [] proj      = {MediaStore.Images.Media.DATA};
+        Cursor cursor       = managedQuery( contentUri, proj, null, null,null);
+
+        if (cursor == null) return null;
+
+        int column_index    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+    }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode != RESULT_OK) return;
+
+            Bitmap bitmap   = null;
+
+            if (requestCode == PICK_FROM_FILE) {
+                mImageCaptureUri = data.getData();
+                path = getRealPathFromURI(mImageCaptureUri); //from Gallery
+
+                if (path == null)
+                    path = mImageCaptureUri.getPath(); //from File Manager
+
+                if (path != null)
+                    bitmap  = BitmapFactory.decodeFile(path);
+            } else {
+                path    = mImageCaptureUri.getPath();
+                bitmap  = BitmapFactory.decodeFile(path);
+            }
+
+            ImagenActual=bitmap;
+        }
+
+    }
