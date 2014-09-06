15f7f9afa9fe98c39aad932fb079fcbe8607282f
Kakrain <wherethesunnoshine@hotmail.com>
2014-09-06 14:09:37 -0500

    añadido mapa + direccion
    mapa en la pantalla de publicacion, y buscar por direccion

--- app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
+++ app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
@@ -3,29 +3,53 @@ package com.ihm.clasificasas.clasificasas;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Denny on 28/07/2014.
  */
-public class publicar extends Activity {
+public class publicar extends FragmentActivity {
 
     Spinner spinner_tipo;
     private Uri mImageCaptureUri;
@@ -37,31 +61,118 @@ public class publicar extends Activity {
             "Venta / Alquiler", "Venta", "Alquiler"
     };
 
+    SupportMapFragment fm;
+    GoogleMap googleMap;
+    MapFragment mMapFragment;
+    MarkerOptions moself=new MarkerOptions();
+    MarkerOptions mohome=new MarkerOptions();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.publicar);
+
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         prepareImagePicker();
-        /*spinner_tipo = (Spinner)findViewById(R.id.publicar_tipo_sp);
-        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tipo);
-        spinner_tipo.setAdapter(adapter4);
-        spinner_tipo.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> arg0, View arg1,int arg2, long arg3) {
-                        //int position = spinner_tipo.getSelectedItemPosition();
-                        //Toast.makeText(getApplicationContext(), "You have selected " + tipo[+position], Toast.LENGTH_LONG).show();
-                    }
-                    @Override
-                    public void onNothingSelected(AdapterView<?> arg0) {
-                        // TODO Auto-generated method stub
-                    }
-                }
-        );*/
-    }
 
+
+        fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        googleMap = fm.getMap();
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        moself.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        moself.title("yo");
+
+        mohome.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.home)));
+        mohome.title("Casa");
+        mohome.draggable(true);
+
+        mostrarPosicionActual();
+
+        Button btn_find = (Button) findViewById(R.id.button);
+
+        // Defining button click event listener for the find button
+        View.OnClickListener findClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting reference to EditText to get the user input location
+                EditText etLocation = (EditText) findViewById(R.id.publicar_ubicacion);
+
+                // Getting user input location
+                String location = etLocation.getText().toString();
+
+                if(location!=null && !location.equals("")){
+                    new GeocoderTask().execute(location);
+                }
+            }
+        };
+
+        // Setting button click event listener for the find button
+        btn_find.setOnClickListener(findClickListener);
+
+
+    }
+    public void mostrarPosicionActual(){
+        // Getting reference to the SupportMapFragment of activity_main.xml
+        // Getting GoogleMap object from the fragment
+        if(!verifygoogleavailability()){
+            return;
+        }
+        // Enabling MyLocation Layer of Google Map
+        googleMap.setMyLocationEnabled(true);
+        // Getting LocationManager object from System Service LOCATION_SERVICE
+        final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+        // Getting the name of the best provider
+        final String provider = locationManager.getBestProvider(criteria, true);
+        // Getting Current Location
+        final Location location = locationManager.getLastKnownLocation(provider);
+        final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // redraw the marker when get location update.
+                setMarker(location, googleMap, moself);
+                refreshMarkers();
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            @Override
+            public void onProviderEnabled(String provider) {}
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
+        if(location!=null){
+            //PLACE THE INITIAL MARKER
+            setMarker(location,googleMap,moself);
+            setMarker(location,googleMap,mohome);
+            refreshMarkers();
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18.0f));
+        locationManager.requestLocationUpdates(provider,20000,0,locationListener);
+    }
+    private void refreshMarkers(){
+        googleMap.clear();
+        googleMap.addMarker(moself);
+        googleMap.addMarker(mohome);
+    }
+    private void setMarker(Location location,GoogleMap googleMap,MarkerOptions mo){
+        LatLng Position = new LatLng(location.getLatitude(),location.getLongitude());
+        mo.position(Position);
+        mo.snippet("Lat:" + location.getLatitude() + "Lng:" + location.getLongitude());
+    }
+    public boolean verifygoogleavailability(){
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        // Showing status
+        if(status!= ConnectionResult.SUCCESS){ // Google Play Services are not available
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+            return false;
+        }
+        return true;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -150,4 +261,58 @@ public class publicar extends Activity {
         }
         ImagenActual=bitmap;
     }
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
+        LatLng latLng;
+        MarkerOptions markerOptions;
+        @Override
+        protected List<Address> doInBackground(String... locationName) {
+            // Creating an instance of Geocoder class
+            Geocoder geocoder = new Geocoder(getBaseContext());
+            List<Address> addresses = null;
+
+            try {
+                // Getting a maximum of 3 Address that matches the input text
+                addresses = geocoder.getFromLocationName(locationName[0], 3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return addresses;
+        }
+
+        @Override
+        protected void onPostExecute(List<Address> addresses) {
+
+            if(addresses==null || addresses.size()==0){
+                Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
+            }
+
+            // Clears all the existing markers on the map
+            googleMap.clear();
+
+            // Adding Markers on Google Map for each matching address
+            for(int i=0;addresses!=null&&i<addresses.size();i++){
+
+                Address address = (Address) addresses.get(i);
+
+                // Creating an instance of GeoPoint, to display in Google Map
+                latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                String addressText = String.format("%s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getCountryName());
+
+                markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(addressText);
+
+                googleMap.addMarker(markerOptions);
+
+                // Locate the first location
+                if(i==0)
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        }
+    }
+
+
 }
