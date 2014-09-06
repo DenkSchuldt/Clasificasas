0b148e47bc1742b24a9606408d0fecafcd8d7c43
DenkSchuldt <dschuldt@outlook.com>
2014-09-06 12:47:23 -0500

    Adding a spinner for the cities

--- app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
+++ app/src/main/java/com/ihm/clasificasas/clasificasas/publicar.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
@@ -74,92 +75,79 @@ public class publicar extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
     public void prepareImagePicker() {
         final String[] items = new String[]{"From Camera", "From SD Card"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, items);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
         builder.setTitle("Select Image");
         builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
-                if (item == 0) {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File file = new File(Environment.getExternalStorageDirectory(),
-                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
-                    mImageCaptureUri = Uri.fromFile(file);
-
-                    try {
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
-                        intent.putExtra("return-data", true);
-
-                        startActivityForResult(intent, PICK_FROM_CAMERA);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    dialog.cancel();
-                } else {
-                    Intent intent = new Intent();
-
-                    intent.setType("image/*");
-                    intent.setAction(Intent.ACTION_GET_CONTENT);
-
-                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+            if (item == 0) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = new File(Environment.getExternalStorageDirectory(),
+                        "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                mImageCaptureUri = Uri.fromFile(file);
+                try {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+                    intent.putExtra("return-data", true);
+                    startActivityForResult(intent, PICK_FROM_CAMERA);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+                dialog.cancel();
+            } else {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+            }
             }
         });
         final AlertDialog dialog = builder.create();
-
-        findViewById(R.id.subir_foto).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.publicar_portada).setOnClickListener(new View.OnClickListener() {
 
             //mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.show();
-                ((ImageView)findViewById(R.id.subir_foto)).setImageBitmap(ImagenActual);
+            dialog.show();
+            ((ImageButton)findViewById(R.id.publicar_portada)).setImageBitmap(ImagenActual);
+            ((ImageButton)findViewById(R.id.publicar_portada)).setVisibility(ImageButton.VISIBLE);
             }
         });
         findViewById(R.id.publicar_fotografias).setOnClickListener(new View.OnClickListener() {
             //mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.show();
+            dialog.show();
             }
         });
     }
+
     public String getRealPathFromURI(Uri contentUri) {
-        String [] proj      = {MediaStore.Images.Media.DATA};
-        Cursor cursor       = managedQuery( contentUri, proj, null, null,null);
-
+        String [] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery( contentUri, proj, null, null,null);
         if (cursor == null) return null;
-
         int column_index    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
         cursor.moveToFirst();
-
         return cursor.getString(column_index);
     }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (resultCode != RESULT_OK) return;
 
-            Bitmap bitmap   = null;
-
-            if (requestCode == PICK_FROM_FILE) {
-                mImageCaptureUri = data.getData();
-                path = getRealPathFromURI(mImageCaptureUri); //from Gallery
-
-                if (path == null)
-                    path = mImageCaptureUri.getPath(); //from File Manager
-
-                if (path != null)
-                    bitmap  = BitmapFactory.decodeFile(path);
-            } else {
-                path    = mImageCaptureUri.getPath();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+        Bitmap bitmap   = null;
+        if (requestCode == PICK_FROM_FILE) {
+            mImageCaptureUri = data.getData();
+            path = getRealPathFromURI(mImageCaptureUri); //from Gallery
+            if (path == null)
+                path = mImageCaptureUri.getPath(); //from File Manager
+            if (path != null)
                 bitmap  = BitmapFactory.decodeFile(path);
-            }
-
-            ImagenActual=bitmap;
+        } else {
+            path = mImageCaptureUri.getPath();
+            bitmap = BitmapFactory.decodeFile(path);
         }
-
+        ImagenActual=bitmap;
     }
+}
